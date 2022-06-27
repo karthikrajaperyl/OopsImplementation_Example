075df701ba563535c5fc9eda33ac883bbf522ba8
karthikrajaperyl <77962533+karthikrajaperyl@users.noreply.github.com>
2022-06-27 11:35:24 +0530

    Add files via upload

---
+++ Exception_Handling/ExceptionHandling.java
@@ -0,0 +1,36 @@
+package Exception_Handling;
+import java.util.*;
+public class ExceptionHandling {
+public void findDivision(int a,int b)
+{
+	if(b==0)
+		throw new ArithmeticException();
+}
+public static void main(String[] args)
+{
+	Scanner scannerObj=new Scanner(System.in);
+	ExceptionHandling tempObj=new ExceptionHandling();
+	try
+	{
+		int a=scannerObj.nextInt();
+		int b=scannerObj.nextInt();
+		tempObj.findDivision(a,b);
+		int age=scannerObj.nextInt();
+	}
+	catch(InputMismatchException e)
+	{
+		e.printStackTrace();
+		System.out.println(e.getMessage());
+	}
+	catch(ArithmeticException e)
+	{
+		e.printStackTrace();
+		System.out.println(e.getMessage());
+	}
+	catch(Exception e)
+	{
+		e.printStackTrace();
+		System.out.println(e.toString());
+	}
+}
+}
